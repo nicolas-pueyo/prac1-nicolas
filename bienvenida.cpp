a87cb38605013ee05c397562426307029d8baefa
nicolas-pueyo <117322546+nicolas-pueyo@users.noreply.github.com>
2022-11-03 16:40:07 +0100

    Add files via upload

---
+++ bienvenida.cpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Curso de Programación 1. Tema 1
+ * Autores: Miguel Ángel Latre y Javier Martínez
+ * Modificado por: Nicolás Pueyo Soria
+ * Última revisión: 18 de septiembre de 2021
+ * Resumen: Tareas 1 y 2
+ ******************************************************************************/
+#include <iostream>
+
+/*
+ * Programa que escribe en la pantalla el mensaje
+ * “Bienvenidos a UNIZAR”
+ */
+int main() {
+    std::cout << "Bienvenidos a UNIZAR" << std::endl;
+    std::cout << "Bienvenidos a Prog1" << std::endl;
+    system("pause");
+}
+
---
+++ eco-numerico.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Curso de Programación 1. Tema 1
+ * Autores: Miguel Ángel Latre
+ * Modificado por: Nicolás Pueyo Soria
+ * Última revisión: 18 de septiembre de 2021
+ * Resumen: Tarea 3 y 4
+ ******************************************************************************/
+#include <iostream>
+using namespace std;
+
+/*
+ * Programa que solicita un número entero al usuario y lo escribe en pantalla.
+ */
+int main() {
+
+    
+    int x;
+    int y;
+    cout << "Escriba dos enteros separados por un espacio: ";
+    cin >> x;
+    cin >> y;
+    
+    cout << "Su suma es: " << x + y << endl;
+    system("pause");
+}
+
